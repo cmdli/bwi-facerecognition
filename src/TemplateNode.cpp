38be775e8d2ef2a1ca989d503d2203a943003815
Chris de la Iglesia <cmdli@utexas.edu>
2013-03-07 18:10:08 -0600

    Created a template node for image operations with OpenCV; Added node to compute X gradients

---
+++ src/TemplateNode.cpp
@@ -0,0 +1,46 @@
+
+#define INPUT_TOPIC "camera/rgb/image_color"
+#define OUTPUT_TOPIC "detector/blurred"
+
+image_transport::Publisher publisher;
+
+void callback(const sensor_msgs::ImageConstPtr &msg)
+{
+  //Load image into OpenCV
+  const sensor_msgs::Image img = *msg;
+  cv_bridge::CvImagePtr image = cv_bridge::toCvCopy(msg);
+  cv::Mat cvImage = image->image;
+  cv::Mat cvOutput; 
+
+
+  
+  //Publish image
+  publisher.publish(image->toImageMsg());
+
+}
+
+int main( int argc, char** argv)
+{
+
+  //Init ROS
+  ros::init(argc, argv, "test_blur");
+  ros::NodeHandle n;
+
+
+  //Set up image publish and subscribe
+  image_transport::ImageTransport it(n);
+
+  image_transport::Subscriber sub = 
+    it.subscribe(INPUT_TOPIC,
+		 1,
+		 callback);
+
+  publisher = it.advertise(OUTPUT_TOPIC,1);
+
+  //Transfer control to ROS
+  ros::spin();
+
+  return 0;
+
+}
+
