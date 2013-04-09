cfe3d9c98b061b167e3546eae989b7dae2ad2ea5
Chris de la Iglesia <cmdli@utexas.edu>
2013-04-09 14:41:49 -0500

    Changed the face detector node to use a launch file for easier configuration; use 'roslaunch friproject face_detector.launch' instead

--- src/FaceDetector.cpp
+++ src/FaceDetector.cpp
@@ -7,12 +7,9 @@
 
 #include <iostream>
 
- #include <opencv2/objdetect/objdetect.hpp>
- #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
-#define INPUT_TOPIC "camera/rgb/image_color"
-#define OUTPUT_TOPIC "detector/faces"
-#define FACE_CASCADE_FILE "haarcascades/haarcascade_frontalface_default.xml"
 
 image_transport::Publisher publisher;
 
@@ -42,9 +39,6 @@ void callback(const sensor_msgs::ImageConstPtr &msg)
     float y1 = faceRects[i].y;
     float y2 = y1 + faceRects[i].height;
     Mat subimg = cvOutput(Range(x1,y1), Range(x2,y2));
-    /*image->image = subimg;
-    image->encoding = "mono8";
-    publisher.publish(image->toImageMsg());*/
   }
   
   image->image = cvOutput;
@@ -67,13 +61,17 @@ int main( int argc, char** argv)
   image_transport::ImageTransport it(n);
 
   image_transport::Subscriber sub = 
-    it.subscribe(INPUT_TOPIC,
+    it.subscribe("input",
 		 1,
 		 callback);
 
-  publisher = it.advertise(OUTPUT_TOPIC,1);
+  publisher = it.advertise("output",1);
 
-  faces.load(FACE_CASCADE_FILE);
+  string face_cascade_file;
+
+  n.getParam("face_cascade_file", face_cascade_file);
+
+  faces.load(face_cascade_file);
 
   //Transfer control to ROS
   ros::spin();
