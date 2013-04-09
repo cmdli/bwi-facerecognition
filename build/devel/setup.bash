98d4187d8e7705fb3c6eaf8eb27df7fa3deb2ba4
Chris de la Iglesia <cmdli@utexas.edu>
2013-04-09 15:10:57 -0500

    Removed build files from repository

--- build/devel/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-. "/nishome/cmdli/ros/rosbuild_ws/class-code/friproject/build/devel/setup.sh"
--- build/devel/setup.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin/cmake/templates/setup.zsh.in
-
-CATKIN_SHELL=zsh
-emulate sh # emulate POSIX
-. "/nishome/cmdli/ros/rosbuild_ws/class-code/friproject/build/devel/setup.sh"
-emulate zsh # back to zsh mode
